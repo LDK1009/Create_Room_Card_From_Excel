a535964fa917d1960c6979f38dcfbfcb41460a41
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-08-05 10:57:42 +0900

    룸카드 스타일링 완료

--- project/src/pages/Main.jsx
+++ project/src/pages/Main.jsx
@@ -56,7 +56,8 @@ const Main = () => {
       const roomArange1 = startRoomNum[0]
         ? `${startRoomNum[0]}(${startPersonnel[0]})-${endRoomNum[0]}(${endPersonnel[0]})`
         : null;
-      const roomArange2 = startRoomNum[1] ? `${startRoomNum[1]}(${startPersonnel[1]})-${endRoomNum[1]}(${endPersonnel[1]})`
+      const roomArange2 = startRoomNum[1]
+        ? `${startRoomNum[1]}(${startPersonnel[1]})-${endRoomNum[1]}(${endPersonnel[1]})`
         : null;
 
       return (
@@ -65,28 +66,33 @@ const Main = () => {
         // 자매 | 10명 | B 801-802(4)
         <>
           <Container>
-            {/* <Img src={cardImg}/> */}
-            <Name>{name}</Name>
-            <StyledTable>
-              <tbody>
-                <tr>
-                  <StyledTh>형제</StyledTh>
-                  <StyledTd>{totalPersonnel[0] || null}</StyledTd>
-                  <StyledTd>
-                    {roomClass[0] || null}
-                    {roomArange1 || null}
-                  </StyledTd>
-                </tr>
-                <tr>
-                  <StyledTh>자매</StyledTh>
-                  <StyledTd>{totalPersonnel[1] || null}</StyledTd>
-                  <StyledTd>
-                    {roomClass[1] || null}
-                    {roomArange2 || null}
-                  </StyledTd>
-                </tr>
-              </tbody>
-            </StyledTable>
+            <CardImg src={cardImg}/>
+            <TextContainer>
+            <ChurchName>{name}</ChurchName>
+            <InfoTextContainer>
+            <SiblingContainer>
+              <Gender>형제</Gender>
+              <TotalPeople>{totalPersonnel[0] || null}</TotalPeople>
+              <RoomClass>
+                {roomClass[0] || null}
+              </RoomClass>
+              <RoomArange>
+                {roomArange1 || null}
+              </RoomArange>
+            </SiblingContainer>
+            <SistersConstainer>
+              <Gender>자매</Gender>
+              <TotalPeople>{totalPersonnel[1] || null}</TotalPeople>
+              <RoomClass>
+                {roomClass[1] || null}
+              </RoomClass>
+              <RoomArange>
+                {roomArange2 || null}
+              </RoomArange>
+            </SistersConstainer>
+            </InfoTextContainer>
+
+            </TextContainer>
           </Container>
         </>
       );
@@ -123,7 +129,7 @@ export default Main;
 
 const Container = styled.div`
   width: 300px;
-  height: 150px;
+  height: 225px;
   background-color: antiquewhite;
   border: 1px solid lightgreen;
   margin-bottom: 20px;
@@ -134,50 +140,68 @@ const Container = styled.div`
   position: relative;
 `;
 
-const Img = styled.img`
-  width: 300px;
-  height: 150;
-`;
-const FlexBox = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-evenly;
-`;
-const Name = styled.div`
-  font-size: 18px;
+const CardImg = styled.img`
+  position: absolute;
+  top:0px;
+  left:0px;
+  width:100%;
+  height:100%;
+`
+const TextContainer = styled.div`
+  position: absolute;
+  width:100%;
+  height:100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-weight:600;
+  font-size:15px;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-items:center;
+`
+
+const ChurchName = styled.div`
+  margin-top:35px;
+  font-size: 20px;
   font-weight: bold;
+  text-align:center;
 `;
 
-const Total = styled.div`
-  font-size: 18px;
-`;
-const Class = styled(Total)``;
-
-const Arange = styled(Total)`
-  font-size: 18px;
-`;
-
-const CardWraper = styled.div`
-  width: 400px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const StyledTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  margin-bottom: 10px;
+const InfoTextContainer = styled.div`
+  margin-top:5px;
+  width:245px;
+  /* background-color:rgba(0,0,0,0.3); */
+`
+const SiblingContainer = styled.div`
+display:flex;
+margin-top:20px;
 `;
 
-const StyledTh = styled.th`
-  border: 1px solid #ddd;
-  padding: 8px;
-  background-color: #f2f2f2;
-  text-align: left;
-`;
+const SistersConstainer = styled(SiblingContainer)``;
 
-const StyledTd = styled.td`
-  border: 1px solid #ddd;
-  padding: 8px;
-  text-align: left;
-`;
+const Gender = styled.div`
+  width:40px;
+  margin-left:20px;
+  text-align:center;
+  /* background-color:rgba(255,0,0,0.3); */
+`
+
+const TotalPeople = styled.div`
+  width:42px;
+  text-align:center;
+  /* background-color:rgba(0,255,0,0.3); */
+  `
+
+const RoomClass = styled.div`
+width:30px;
+text-align:center;
+/* background-color:rgba(0,0,255,0.3); */
+`
+
+const RoomArange = styled.div`
+width:100px;
+text-align:center;
+/* background-color:rgba(255,0,0,0.3);   */
+`
